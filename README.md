74b86029e351c0734ac7bfc12fe99ad2a6c1dd54
Arijit Chowdhury <arishubho2@outlook.com>
2024-09-14 22:13:15 +0530

    add: First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Animation Navbar - (HTML, CSS, JS, GSAP)
+
+### Created by: Arijit Chowdhury
+
+#
+#
+
+Site is live at: 
---
+++ app.js
@@ -0,0 +1,33 @@
+const menu = document.querySelector('#nav i');
+const cross = document.querySelector('#full i');
+
+let tline = gsap.timeline();
+
+tline.to('#full', {
+    right: '0px',
+    duration: 0.28,
+    ease: 'power2.out',
+});
+
+tline.from("#full h4", {
+    x : 150,
+    ease: 'power2.out',
+    stagger: 0.2,
+    opacity: 0,
+   
+});
+
+tline.to("#ani", {
+    opacity: 1,
+})
+
+
+tline.pause();
+
+menu.addEventListener('click', ()=>{
+    tline.play(); 
+})
+
+cross.addEventListener('click', ()=>{
+    tline.reverse();
+})
